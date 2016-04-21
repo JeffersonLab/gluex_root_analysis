1e5bb9cb44261ac3ed9153d5c5ae3bbb99e6a938
pmattjlab <pmatt@jlab.org>
2016-04-21 13:46:03 -0400

    Improve build system.

--- programs/MakePROOFPackage/build.sh
+++ programs/MakePROOFPackage/build.sh
@@ -9,29 +9,29 @@ if [  -d "$DIRNAME" ]; then
 fi
 
 rm -rf ${DIRNAME}
-rm -rf ${ROOT_ANALYSIS_HOME}/${OS_NAME}/packages/${PACKAGENAME}.par
+rm -rf ${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/packages/${PACKAGENAME}.par
 mkdir ${DIRNAME}
-mkdir -p ${ROOT_ANALYSIS_HOME}/${OS_NAME}/packages/
+mkdir -p ${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/packages/
 
 #PROOF-INF
 mkdir ${DIRNAME}/PROOF-INF
 cp SETUP.C ${DIRNAME}/PROOF-INF/
 
 #LIBRARIES
-cp ${ROOT_ANALYSIS_HOME}/${OS_NAME}/lib/libDSelector.so ${DIRNAME}/
+cp ${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/lib/libDSelector.so ${DIRNAME}/
 
 #HEADERS
-cp -r ${ROOT_ANALYSIS_HOME}/${OS_NAME}/include/DSelector ${DIRNAME}/
+cp -r ${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/include/DSelector ${DIRNAME}/
 cp $HALLD_HOME/$BMS_OSNAME/include/particleType.h ${DIRNAME}/
 
 # build archive
 tar -czf ${PACKAGENAME}.par ${DIRNAME}
 
 # install
-mv ${PACKAGENAME}.par ${ROOT_ANALYSIS_HOME}/${OS_NAME}/packages/
+mv ${PACKAGENAME}.par ${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/packages/
 
 # cleanup
 rm -rf ${DIRNAME}
 
-echo ${PACKAGENAME}.par built and installed to ${ROOT_ANALYSIS_HOME}/${OS_NAME}/packages/
+echo ${PACKAGENAME}.par built and installed to ${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/packages/
 
