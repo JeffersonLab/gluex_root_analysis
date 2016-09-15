8fb2002a262212e54a0245f926be0884c6bbfba4
pmattjlab <pmatt@jlab.org>
2016-09-15 14:46:11 -0400

    Add function to query CCDB for beam bunch period.

--- libraries/DSelector/DAnalysisUtilities.h
+++ libraries/DSelector/DAnalysisUtilities.h
@@ -24,6 +24,8 @@ class DAnalysisUtilities
 		TLorentzVector Calc_FinalStateP4(const DParticleCombo* locParticleComboWrapper, size_t locStepIndex, set<size_t> locToIncludeIndices, map<Particle_t, set<Int_t> >& locSourceObjects, bool locUseKinFitDataFlag) const;
 
 		bool Get_IsPolarizedBeam(int locRunNumber, bool& locIsPARAFlag) const; //RCDB environment must be setup!!
+		double Get_BeamBunchPeriod(int locRunNumber) const; //CCDB environment must be setup!!
+
 		double Calc_ProdPlanePhi_Pseudoscalar(double locBeamEnergy, Particle_t locTargetPID, const TLorentzVector& locMesonP4) const;
 		double Calc_DecayPlanePsi_Vector_3BodyDecay(double locBeamEnergy, Particle_t locTargetPID, const TLorentzVector& locBaryonP4, const TLorentzVector& locMesonP4, const TLorentzVector& locMesonProduct1P4, const TLorentzVector& locMesonProduct2P4, double& locDecayPlaneTheta) const;
 
