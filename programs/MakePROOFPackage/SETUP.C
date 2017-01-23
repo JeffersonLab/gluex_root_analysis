01fe9761a95b000218b9b36cdd0fa41e017ac1e3
pmattjlab <pmatt@jlab.org>
2017-01-23 17:32:54 -0500

    For some reason, before this fix, PROOF could not find the header files necessary to build your selector (if not created yet). Even though they are present in the package, and the include path was pointing to them.
    Whatever. Instead, explicitly include the include paths when loading on
    each worker thread.

--- programs/MakePROOFPackage/SETUP.C
+++ programs/MakePROOFPackage/SETUP.C
@@ -1,5 +1,11 @@
 Int_t SETUP()
 {
 	gSystem->Load("./libDSelector.so");
+	gSystem->AddIncludePath("-I.");
+	gSystem->AddIncludePath("-I${HALLD_HOME}/${BMS_OSNAME}/include/");
+	gSystem->AddIncludePath("-I${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/include/");
+	std::cout << "hello?" << std::endl;
+	std::cout << "pwd: " << gSystem->pwd() << std::endl;
+	std::cout << "INCLUDE PATH: " << gSystem->GetIncludePath() << std::endl;
 	return 0;
 }
