41e3de101ce2e3ac4809e40964eab50ecf50af56
pmattjlab <pmatt@jlab.org>
2017-01-23 17:34:41 -0500

    Delete comment lines.

--- programs/MakePROOFPackage/SETUP.C
+++ programs/MakePROOFPackage/SETUP.C
@@ -4,8 +4,5 @@ Int_t SETUP()
 	gSystem->AddIncludePath("-I.");
 	gSystem->AddIncludePath("-I${HALLD_HOME}/${BMS_OSNAME}/include/");
 	gSystem->AddIncludePath("-I${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/include/");
-	std::cout << "hello?" << std::endl;
-	std::cout << "pwd: " << gSystem->pwd() << std::endl;
-	std::cout << "INCLUDE PATH: " << gSystem->GetIncludePath() << std::endl;
 	return 0;
 }
