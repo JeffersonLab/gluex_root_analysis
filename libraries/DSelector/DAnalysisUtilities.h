45c3014727ee9cd7f4201b4b868791bc7dd4fa65
Alex Austregesilo <austreg@jlab.org>
2017-04-14 17:40:06 -0400

    new function to access polarization angle from rcdb

--- libraries/DSelector/DAnalysisUtilities.h
+++ libraries/DSelector/DAnalysisUtilities.h
@@ -24,6 +24,7 @@ class DAnalysisUtilities
 		TLorentzVector Calc_FinalStateP4(const DParticleCombo* locParticleComboWrapper, size_t locStepIndex, set<size_t> locToIncludeIndices, map<unsigned int, set<Int_t> >& locSourceObjects, bool locUseKinFitDataFlag) const;
 
 		bool Get_IsPolarizedBeam(int locRunNumber, bool& locIsPARAFlag) const; //RCDB environment must be setup!!
+		bool Get_PolarizationAngle(int locRunNumber, int& locPolarizationAngle) const; //RCDB environment must be setup!!
 		double Get_BeamBunchPeriod(int locRunNumber) const; //CCDB environment must be setup!!
 
 		double Calc_ProdPlanePhi_Pseudoscalar(double locBeamEnergy, Particle_t locTargetPID, const TLorentzVector& locMesonP4) const;
