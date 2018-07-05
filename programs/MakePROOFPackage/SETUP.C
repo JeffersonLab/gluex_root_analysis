350e763a52a44d17f3cf2e2c88262de5342e77fd
markito3 <marki@jlab.org>
2018-07-05 18:24:54 -0400

    More changes to work with halld_recon.

--- programs/MakePROOFPackage/SETUP.C
+++ programs/MakePROOFPackage/SETUP.C
@@ -2,7 +2,7 @@ Int_t SETUP()
 {
 	gSystem->Load("./libDSelector.so");
 	gSystem->AddIncludePath("-I.");
-	gSystem->AddIncludePath("-I${HALLD_HOME}/${BMS_OSNAME}/include/");
+	gSystem->AddIncludePath("-I${HALLD_RECON_HOME}/${BMS_OSNAME}/include/");
 	gSystem->AddIncludePath("-I${ROOT_ANALYSIS_HOME}/${BMS_OSNAME}/include/");
 	return 0;
 }
