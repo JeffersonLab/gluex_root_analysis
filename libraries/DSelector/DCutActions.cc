7296552384417d70028b020eb491e7419327c19b
Naomi Jarvis <nsj@cmu.edu>
2021-06-14 20:00:40 -0400

    compiles

--- libraries/DSelector/DCutActions.cc
+++ libraries/DSelector/DCutActions.cc
@@ -471,18 +471,18 @@ string DCutAction_dEdx::Get_ActionName(void) const
 
 void DCutAction_dEdx::Initialize(void)
 {
-	if(dFunc_dEdxCut_SelectHeavy == NULL)
+	if(dFunc_dEdxCut_Max == NULL)
 	{
-		string locFuncName = "df_dEdxCut_SelectHeavy"; //e.g. proton
-		dFunc_dEdxCut_SelectHeavy = new TF1(locFuncName.c_str(), "exp(-1.0*[0]*x + [1]) + [2]", 0.0, 12.0);
-		dFunc_dEdxCut_SelectHeavy->SetParameters(dSelectHeavy_c0, dSelectHeavy_c1, dSelectHeavy_c2);
+		string locFuncName = "df_dEdxCut_Max"; //e.g. proton
+		dFunc_dEdxCut_Max = new TF1(locFuncName.c_str(), "exp(-1.0*[0]*x + [1]) + [2]", 0.0, 12.0);
+		dFunc_dEdxCut_Max->SetParameters(dMax_c0, dMax_c1, dMax_c2);
 	}
 
-	if(dFunc_dEdxCut_SelectLight == NULL)
+	if(dFunc_dEdxCut_Min == NULL)
 	{
-		string locFuncName = "df_dEdxCut_SelectLight"; //e.g. pions, kaons
-		dFunc_dEdxCut_SelectLight = new TF1(locFuncName.c_str(), "exp(-1.0*[0]*x + [1]) + [2]", 0.0, 12.0);
-		dFunc_dEdxCut_SelectLight->SetParameters(dSelectLight_c0, dSelectLight_c1, dSelectLight_c2);
+		string locFuncName = "df_dEdxCut_Min"; //e.g. pions, kaons
+		dFunc_dEdxCut_Min = new TF1(locFuncName.c_str(), "exp(-1.0*[0]*x + [1]) + [2]", 0.0, 12.0);
+		dFunc_dEdxCut_Min->SetParameters(dMin_c0, dMin_c1, dMin_c2);
 	}
 }
 
@@ -529,28 +529,12 @@ bool DCutAction_dEdx::Perform_Action(void)
 
 			//cut
 			double locP = dUseKinFitFlag ? locChargedTrackHypothesis->Get_P4().Vect().Mag() : locChargedTrackHypothesis->Get_P4_Measured().Vect().Mag();
-			if((ParticleMass(locPID) + 0.0001) >= ParticleMass(Proton))
-			{
-				//heavy
-				if(dMaxRejectionFlag) //focus on rejecting background pions
-				{
-					if(locdEdx < dFunc_dEdxCut_SelectLight->Eval(locP))
-						return false;
-				}
-				else if(locdEdx < dFunc_dEdxCut_SelectHeavy->Eval(locP))
+				if(locdEdx < dFunc_dEdxCut_Min->Eval(locP))
+					return false;
+
+				if(locdEdx > dFunc_dEdxCut_Max->Eval(locP))
 					return false; //focus on keeping signal protons
-			}
-			else
-			{
-				//light
-				if(dMaxRejectionFlag) //focus on rejecting background pions
-				{
-					if(locdEdx > dFunc_dEdxCut_SelectHeavy->Eval(locP))
-						return false;
-				}
-				else if(locdEdx > dFunc_dEdxCut_SelectLight->Eval(locP))
-					return false; //focus on keeping signal pions
-			}
+
 		} //end of particle loop
 	} //end of step loop
 
